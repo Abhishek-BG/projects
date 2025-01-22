50c35f33535493e3e95db23ce0414f723bd1d85c
Abhishek-BG <abhigowda0190@gmail.com>
2025-01-23 02:37:56 +0530

    updates

---
+++ Problem-3C-3/server/main.js
@@ -0,0 +1,34 @@
+// Load the dotenv package
+require('dotenv').config();
+
+// Access environment variables
+const port = process.env.PORT || 3000;
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const apiKey = process.env.API_KEY;
+
+// A function to demonstrate the use of environment variables
+function connectToDatabase() {
+  console.log(`Connecting to database on host: ${dbHost}`);
+  console.log(`Using credentials: ${dbUser} : ${dbPass.length}`);
+}
+
+function useApiKey() {
+  console.log(`Using API Key: ${apiKey}`)
+}
+
+// Start a simple server
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the dotenv example application!');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+  connectToDatabase();
+  useApiKey();
+});
